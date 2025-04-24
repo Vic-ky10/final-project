ea83b58036ae51e8ee78bd7a140a29e9cadb4734
vic-ky10 <vigneshvenketasan@gmail.com>
2025-04-24 14:53:40 +0530

    project completed

---
+++ backend/server.js
@@ -0,0 +1,4 @@
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}));
---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  }
+});
+
+// Add request interceptor to include token
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("authToken");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
